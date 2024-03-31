4d8c41aff782b7d6fececf912afc2688ed553429
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-03-31 15:37:59 +0300

    New description

--- Zadanie2.1.cpp
+++ Zadanie2.1.cpp
@@ -1,49 +1,95 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <locale.h>
+#include <map>
+
 using namespace std;
-
-vector<string> charRomanNumeral = {"I","V","X","L","C","D","M"};
-vector<int> numberRomanNumeral = { 1,5,10,50,100,500,1000 };
-
-bool isValidRomanNumeral(string validLineRomanNumeral);// Проверка римского числа
+bool isValidRomanNumeral(string romanNumber);
+int translation(string romanNumber);
+map <int, string> RomanNumeral = { {1,"I"},{5,"V"},{10,"X"},{50,"L"},{100,"C"},{500,"D"},{1000,"M"} };
+map <string,int> RomanNumeralReverse = { {"I",1},{"V",5} ,{"X",10} ,{"L",50} ,{"C",100} ,{"D",500} ,{"M",1000} };
 
 int main(void)
 {
-	setlocale(LC_ALL, "Rus");
-	string lineRomanNumeral;// вводимое римское число
-	do
+	string number;
+	while (true)
 	{
-		cout << "Enter Roman numbers:" << endl;
-		cin >> lineRomanNumeral;
-			
-	} while (isValidRomanNumeral(lineRomanNumeral) == false);
-	
+		do
+		{
+			cout << "(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)\nEnter roman number:" << endl;
+			cin >> number;
+			if (isValidRomanNumeral(number) == false)
+			{
+				cout << "Error roman number\n" << endl;
+			}
+		} while (isValidRomanNumeral(number) == false);
+		int sum1 = 0;
+		sum1 = translation(number);
+		cout << "Arabian number = " << sum1 << endl;
+	}
+
 	return 0;
 }
-	
-bool isValidRomanNumeral(string validLineRomanNumeral)
+
+bool isValidRomanNumeral(string romanNumber)
 {
-	for (int i = 0; i < validLineRomanNumeral.size(); i++)// Проверка на используемые символы
+	int check = 0;
+	int counter = 0;
+	for (int i = 0; i < romanNumber.size(); i++)// проверка на использование допустимых символов
 	{
 		int counter = 0;
-		for (int j = 0; j < charRomanNumeral.size(); j++)
+		for (int j = 0; j < RomanNumeral.size(); j++)
 		{
-			if (validLineRomanNumeral.substr(i,1) == charRomanNumeral[j])
+			if (romanNumber.substr(i, 1) == RomanNumeral[j])
 			{
 				counter++;
 				break;
 			}
 		}
-		if (counter == 0)
-			return false;
+		if (counter == 0) return false;
 	}
-	for (int i = 0; i < validLineRomanNumeral.size(); i++)// Проверка чтобы L V D не повторялись больше 2 раз
+	for (int i = 0; i < romanNumber.size(); i++)// недопустимое использование (L = 50, D = 500, V = 5) 2 раза и более
 	{
-		if(validLineRomanNumeral.substr(i,1) == charRomanNumeral[1] 
-			
-	
+		if (romanNumber.substr(i, 1) == RomanNumeral[5] && romanNumber.substr(i + 1, 1) == RomanNumeral[5])
+		{
+			check++;
+		}
+		else if (romanNumber.substr(i, 1) == RomanNumeral[50] && romanNumber.substr(i + 1, 1) == RomanNumeral[50])
+		{
+			check++;
+		}
+		else if (romanNumber.substr(i, 1) == RomanNumeral[500] && romanNumber.substr(i + 1, 1) == RomanNumeral[500])
+		{
+			check++;
+		}
+		if (check > 0) return 0;
+	}
+	for (int i = 0; i < romanNumber.size(); i++)// повторение более 3 раз 
+	{
+		check = 0;
+		for (int j = 0; j < romanNumber.size(); j++)
+		{
+			if (romanNumber.substr(i, 1) == romanNumber.substr(j, 1)) check++;
+			else break;
+		}
+		if (check > 3) return false;
+	}
 	return true;
 }
 
+int translation(string romanNumber)//перевод числа в арабское
+{
+	int total = 0;
+	int prevValue = 0;
+	for (int i = romanNumber.size(); i >= 0; i--)//бегаем по введеному римскому числу
+	{
+		int value = RomanNumeralReverse[romanNumber.substr(i, 1)];
+		if (value < prevValue)
+			total -= value;
+		else
+			total += value;
+		prevValue = value;
+	}
+
+	return total;
+}
--- Zadanie3.21.cpp
+++ Zadanie3.21.cpp
@@ -7,16 +7,14 @@ int main(void)
 	cin >> S;
 	int counter = 0;
 	int index1left = 0;
-	int index1Rigth = 0;
 	for (int i = 0; i < S.size(); i++)
 	{
 		if (S.substr(i, 1) == "0")
 		{
 			counter++;// счетчик нулей
-		} 
-		int index1left = i - counter;// индекс левой единицы
-		int index1Rigth = i + 1;// индекс правой единицы
-		if (S.substr(index1left, 1) == "1" && S.substr(index1Rigth, 1) == "1" && S.substr(i, 1) == "0")// условие проверки 1[0]1
+		}
+		index1left = i - counter;// индекс левой единицы
+		if (S.substr(index1left, 1) == "1" && S.substr(i+1, 1) == "1" && S.substr(i, 1) == "0")// условие проверки 1[0]1
 		{
 			cout << S.substr(index1left, counter + 2) << endl;
 			counter = 0;
