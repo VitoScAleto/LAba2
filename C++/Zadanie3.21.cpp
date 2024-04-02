33d67f7f5916f117199ac772163d4cf4a30b206d
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-02 19:44:33 +0300

    minor changes

--- C++/Zadanie3.21.cpp
+++ C++/Zadanie3.21.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main(void)
 {
-	string S;
-	cin >> S;
-	int counter = 0;
-	int index1left = 0;
-	for (int i = 0; i < S.size(); i++)
+	int counter1 = 0;
+	int cpunter2 = 0;
+	int k = 0;
+	string S = "101vsjkl100ca1001o1,d";
+	//cin >> S;
+	vector <int> UnitIndex;
+	for (int i = 0; i < S.size(); i++ )// find index unit
 	{
-		if (S.substr(i, 1) == "0")
+		if (S.substr(i, 1) == "1")// find index unit
 		{
-			counter++;// счетчик нулей
+			UnitIndex.push_back(i);// add index in set
 		}
-		index1left = i - counter;// индекс левой единицы
-		if (S.substr(index1left, 1) == "1" && S.substr(i+1, 1) == "1" && S.substr(i, 1) == "0")// условие проверки 1[0]1
+	}
+	//for (auto x : UnitIndex)
+	//{
+	//	cout << x << endl;
+	//}
+	for (int i = 0; i < UnitIndex.size(); i++)// i = index unit
+	{
+		int substringLength = UnitIndex[i + 1] - UnitIndex[i] + 1;// substring length 1[0]1
+		if (substringLength % 2 == 0)// проверка четности числа
 		{
-			cout << S.substr(index1left, counter + 2) << endl;
-			counter = 0;
+			for (int j = UnitIndex[i + 1], k = i; j < substringLength; j--,k++)
+			{
+				if (S.substr(k + 1, 1) == "0" && S.substr(j - 1, 1) == "0")
+				{
+					counter1++;
+				}
+			}
 		}
+		k = 0;
+		if (counter1 == (substringLength - 2) % 2) cout << S.substr(i, substringLength);
+		//if (substringLength % 2 == 1)// проверка четности числа
+		//{
+		//	for (int j = UnitIndex.count(i + 1); j < substringLength; j--,i++)
+		//	{
+		//		if (S.substr(i + 1, 1) == "0" && S.substr(j - 1, 1) == "0");
+		//		
+		//	}
+		//}
 		
+
 	}
 	return 0;
 }
