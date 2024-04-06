0f0323b7c9a61e817333e5e44908798fc7c7717b
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-06 22:31:51 +0300

    working code GO

--- GO/main.go
+++ GO/main.go
@@ -1,32 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
-func main() {
-	Zadanie1_1()
-	Zadanie2_1()
-	Zadanie3_21()
-}
-
 func Zadanie1_1() {
 	numbers := []int{76, 24, 614, 14, 1153}
-	for i, a := range numbers {
-		if a < 0 {
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] < 0 {
 			break
 		}
+		a := numbers[i]
 		sum := 0
 		mult := 1
 		for a > 0 {
 			b := a % 10
 			sum += b
 			mult *= b
-			a /= 10
+			a = a / 10
 		}
 		if sum < mult {
 			fmt.Printf("Index number = %d\n", i)
+		}
+	}
+}
+
+func Zadanie3_21() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		counterNull := 1
+		fmt.Println("Enter the string:")
+		S, _ := reader.ReadString('\n')
+		S = strings.TrimSpace(S)
+		for i := 0; i < len(S)-1; i++ {
+			if S[i:i+1] == "0" && S[i+1:i+2] == "0" {
+				counterNull++
+			}
+			if i < len(S)-2 && S[i+1:i+2] == "1" && S[i:i+1] == "0" {
+				if i-counterNull >= 0 && S[i-counterNull:i-counterNull+1] == "1" && S[i-counterNull+1:i-counterNull+2] == "0" {
+					fmt.Println(S[i-counterNull : i-counterNull+counterNull+2])
+					counterNull = 1
+				}
+			}
 		}
 	}
 }
@@ -42,6 +60,7 @@ func Zadanie2_1() {
 		1000:  "M",
 		10000: "Q",
 	}
+
 	RomanNumeralReverse := map[string]int{
 		"I": 1,
 		"V": 5,
@@ -52,38 +71,42 @@ func Zadanie2_1() {
 		"M": 1000,
 		"Q": 10000,
 	}
-	var number string
+
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		for {
 			fmt.Println("(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)")
 			fmt.Println("Enter roman number:")
-			fmt.Scanln(&number)
-			if !isValidRomanNumeral(number, RomanNumeral) {
+			number, _ := reader.ReadString('\n')
+			number = strings.TrimSpace(number)
+			if !isValidRomanNumeral(number, RomanNumeral, RomanNumeralReverse) {
 				fmt.Println("Error roman number")
 			} else {
+				fmt.Printf("Arabian number = %d\n", translation(number, RomanNumeralReverse))
 				break
 			}
 		}
-		fmt.Printf("Arabian number = %d\n", translation(number, RomanNumeralReverse))
 	}
 }
 
-func isValidRomanNumeral(romanNumber string, RomanNumeral map[int]string) bool {
-	for _, v := range romanNumber {
-		isValidCounter := false
-		for _, r := range RomanNumeral {
-			if string(v) == r {
-				isValidCounter = true
+func isValidRomanNumeral(romanNumber string, RomanNumeral map[int]string, RomanNumeralReverse map[string]int) bool {
+	counterForTwo := 0
+	for i := 0; i < len(romanNumber); i++ {
+		isValidCounter := 0
+		for _, value := range RomanNumeral {
+			if romanNumber[i:i+1] == value {
+				isValidCounter++
 				break
 			}
 		}
-		if !isValidCounter {
+		if isValidCounter == 0 {
 			return false
 		}
 	}
-	counterForTwo := 0
 	for i := 0; i < len(romanNumber)-1; i++ {
-		if romanNumber[i:i+2] == RomanNumeral[5] || romanNumber[i:i+2] == RomanNumeral[50] || romanNumber[i:i+2] == RomanNumeral[500] {
+		if (romanNumber[i:i+1] == RomanNumeral[5] && romanNumber[i+1:i+2] == RomanNumeral[5]) ||
+			(romanNumber[i:i+1] == RomanNumeral[50] && romanNumber[i+1:i+2] == RomanNumeral[50]) ||
+			(romanNumber[i:i+1] == RomanNumeral[500] && romanNumber[i+1:i+2] == RomanNumeral[500]) {
 			counterForTwo++
 		}
 		if counterForTwo > 0 {
@@ -91,7 +114,7 @@ func isValidRomanNumeral(romanNumber string, RomanNumeral map[int]string) bool {
 		}
 		repeatCountThreeTimes := 0
 		for j := i; j < len(romanNumber); j++ {
-			if romanNumber[i] == romanNumber[j] {
+			if romanNumber[i:i+1] == romanNumber[j:j+1] {
 				repeatCountThreeTimes++
 			} else {
 				break
@@ -108,7 +131,7 @@ func translation(romanNumber string, RomanNumeralReverse map[string]int) int {
 	total := 0
 	prevValue := 0
 	for i := len(romanNumber) - 1; i >= 0; i-- {
-		value := RomanNumeralReverse[string(romanNumber[i])]
+		value := RomanNumeralReverse[romanNumber[i:i+1]]
 		if value < prevValue {
 			total -= value
 		} else {
@@ -119,17 +142,24 @@ func translation(romanNumber string, RomanNumeralReverse map[string]int) int {
 	return total
 }
 
-func Zadanie3_21() {
+func main() {
 	for {
-		var S string
-		fmt.Println("Enter the string:")
-		fmt.Scanln(&S)
-		substrings := strings.Split(S, "10")
-		for _, sub := range substrings {
-			if strings.HasPrefix(sub, "1") {
-				fmt.Println("1" + sub)
-			}
+		fmt.Println("Input Zadanie number")
+		fmt.Println("1 - Zadanie1_1")
+		fmt.Println("2 - Zadanie2_1")
+		fmt.Println("3 - Zadanie3_21")
+		fmt.Print("Enter number: ")
+		var s string
+		fmt.Scanln(&s)
+		switch s {
+		case "3":
+			Zadanie3_21()
+		case "2":
+			Zadanie2_1()
+		case "1":
+			Zadanie1_1()
+		default:
+			fmt.Println("Error")
 		}
-		S = ""
 	}
 }
