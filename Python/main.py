db0453f622bfac44b12c8290b2eef12933a5df2f
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-06 22:20:38 +0300

    working code python

--- Python/main.py
+++ Python/main.py
@@ -1,84 +1,82 @@
-def Zadanie3_21():
-    while True:
-        counterNull = 1
-        S = input("Enter the string:\n")
-        for i in range(len(S)):
-            if S[i:i+2] == "00":
-                counterNull += 1
-            if S[i+1:i+2] == "1" and S[i:i+1] == "0":
-                if S[i-counterNull:i-counterNull+1] == "1" and S[i-counterNull+1:i-counterNull+2] == "0":
-                    print(S[i-counterNull:i-counterNull+counterNull+2])
-                    counterNull = 1
-        S = ""
-
-def Zadanie1_1():
-    number = [76, 24, 614, 14, 1153]
-    for i in range(len(number)):
-        if number[i] < 0:
+def zadanie1_1():
+    numbers = [76, 24, 614, 14, 1153]
+    for i in range(len(numbers)):
+        if numbers[i] < 0:
             break
-        a = number[i]
-        sum = 0
+        a = numbers[i]
+        sum_val = 0
         mult = 1
         while a > 0:
             b = a % 10
-            sum += b
+            sum_val += b
             mult *= b
-            a //= 10
-        if sum < mult:
+            a = a // 10
+        if sum_val < mult:
             print("Index number =", i)
-            
-def isValidRomanNumeral(romanNumber, RomanNumeral, RomanNumeralReverse):
-    counterForTwo = 0
-    for i in range(len(romanNumber)):
-        isValidCounter = 0
-        for j in range(len(RomanNumeral)):
-            if romanNumber[i:i+1] == RomanNumeral[j]:
-                isValidCounter += 1
+
+def zadanie3_21():
+    while True:
+        counter_null = 1
+        S = input("Enter the string: ")
+        for i in range(len(S) - 1):
+            if S[i] == '0' and S[i + 1] == '0':
+                counter_null += 1
+            if i < len(S) - 2 and S[i + 1] == '1' and S[i] == '0':
+                if i - counter_null >= 0 and S[i - counter_null] == '1' and S[i - counter_null + 1] == '0':
+                    print(S[i - counter_null:i - counter_null + counter_null + 2])
+                    counter_null = 1
+
+def zadanie2_1():
+    roman_numeral = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M', 10000: 'Q'}
+    roman_numeral_reverse = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'Q': 10000}
+    while True:
+        print("(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)")
+        number = input("Enter roman number: ")
+        while not is_valid_roman_numeral(number, roman_numeral, roman_numeral_reverse):
+            print("Error roman number")
+            number = input("Enter roman number: ")
+        print("Arabian number =", translation(number, roman_numeral_reverse))
+
+def is_valid_roman_numeral(roman_number, roman_numeral, roman_numeral_reverse):
+    counter_for_two = 0
+    for i in range(len(roman_number)):
+        is_valid_counter = 0
+        for key, value in roman_numeral.items():
+            if roman_number[i] == value:
+                is_valid_counter += 1
                 break
-        if isValidCounter == 0:
+        if is_valid_counter == 0:
             return False
-    for i in range(len(romanNumber)):
-        if romanNumber[i:i+1] == RomanNumeral[5] and romanNumber[i+1:i+2] == RomanNumeral[5]:
-            counterForTwo += 1
-        elif romanNumber[i:i+1] == RomanNumeral[50] and romanNumber[i+1:i+2] == RomanNumeral[50]:
-            counterForTwo += 1
-        elif romanNumber[i:i+1] == RomanNumeral[500] and romanNumber[i+1:i+2] == RomanNumeral[500]:
-            counterForTwo += 1
-        if counterForTwo > 0:
+    for i in range(len(roman_number) - 1):
+        if (roman_number[i] == roman_numeral[5] and roman_number[i + 1] == roman_numeral[5]) or \
+           (roman_number[i] == roman_numeral[50] and roman_number[i + 1] == roman_numeral[50]) or \
+           (roman_number[i] == roman_numeral[500] and roman_number[i + 1] == roman_numeral[500]):
+            counter_for_two += 1
+        if counter_for_two > 0:
             return False
-        repeatCountThreeTimes = 0
-        for j in range(len(romanNumber)):
-            if romanNumber[i:i+1] == romanNumber[j:j+1]:
-                repeatCountThreeTimes += 1
+        repeat_count_three_times = 0
+        for j in range(i, len(roman_number)):
+            if roman_number[i] == roman_number[j]:
+                repeat_count_three_times += 1
             else:
                 break
-        if repeatCountThreeTimes > 3:
+        if repeat_count_three_times > 3:
             return False
     return True
 
-def translation(romanNumber, RomanNumeralReverse):
+def translation(roman_number, roman_numeral_reverse):
     total = 0
-    prevValue = 0
-    for i in range(len(romanNumber)-1, -1, -1):
-        value = RomanNumeralReverse[romanNumber[i:i+1]]
-        if value < prevValue:
+    prev_value = 0
+    for i in range(len(roman_number) - 1, -1, -1):
+        value = roman_numeral_reverse[roman_number[i]]
+        if value < prev_value:
             total -= value
         else:
             total += value
-        prevValue = value
+        prev_value = value
     return total
 
-def Zadanie2_1():
-    RomanNumeral = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M", 10000: "Q"}
-    RomanNumeralReverse = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000, "Q": 10000}
-    while True:
-        number = input("(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)\nEnter roman number:\n")
-        if not isValidRomanNumeral(number, RomanNumeral, RomanNumeralReverse):
-            print("Error roman number")
-            continue
-        print("Arabian number =", translation(number, RomanNumeralReverse))
-
-def main():
+if __name__ == "__main__":
     while True:
         print("Input Zadanie number")
         print("1 - Zadanie1_1")
@@ -86,13 +84,10 @@ def main():
         print("3 - Zadanie3_21")
         s = input("Enter number: ")
         if s == '3':
-            Zadanie3_21()
+            zadanie3_21()
         elif s == '2':
-            Zadanie2_1()
+            zadanie2_1()
         elif s == '1':
-            Zadanie1_1()
+            zadanie1_1()
         else:
             print("Error")
-
-if __name__ == "__main__":
-    main()
