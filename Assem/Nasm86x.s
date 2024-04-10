33ac32ec7edbd57c55bac4b86c09d2e6ce387b89
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-10 09:22:01 +0300

    Работает

--- Assem/Nasm86x.s
+++ Assem/Nasm86x.s
@@ -1,5 +1,5 @@
 section .data
-    number dd 15, 24, 614, 14, 1153
+    number dd 14, 25,15, 1381, 
     format db "Index number = %d", 10, 0
 
 section .text
@@ -7,43 +7,47 @@ section .text
     extern printf
 
 main:
+    mov ebp, esp; for correct debugging
   
     mov edi, 0  ; a = 0
     mov esi, 0  ; b = 0
-    mov edx, 0  ; sum = 0
+    mov edx, 0  ; остаток от деления
     mov ecx, 1  ; mult = 1
-
+    mov ebp, 0
     mov ebx, 0  ; i = 0
 loop_start:
-    cmp ebx, 5  ;если счетчик больше кол-во чисел программа завершается 
+    cmp ebx, 4 ;если счетчик больше кол-во чисел программа завершается 
     jnl loop_end  
 
     mov eax, dword [number + ebx*4]  ; load number[i] into eax ebx счетчик
     test eax, eax  ; check if number[i] is negative
     js inner_loop_end  ; if negative, exit loop
 
-    mov edi, eax  ; a = number[i]
 sum_loop:
-    movzx ecx, si  ; load b into ecx
+    mov edi, eax ;делимое
+    mov esi, 10 ;делитель
+    div esi; esi остаток от деления
+    add ebp,edx   ; сумма
+    imul ecx, edx  ; произведение 
+    xor edx, edx   
+    cmp eax, 0  ; 
+    ja sum_loop   ; 
+sravnenie:
+    cmp ecx, ebp; compare sum and mult
+    jl inner_loop_end  ; 
     
-    add eax, ecx  ; sum = sum + b
-    imul ecx, ecx  ; mult = mult * b
-    imul edx, ecx  ; multiply carry by b
-    mov esi, esi  ; decrement b
-    cmp esi, 0  ; check if b != 0
-    jl sum_loop  ; if b != 0, continue sum_loop
-
-    cmp eax, edx  ; compare sum and mult
-    jb inner_loop_end  ; if sum >= mult, exit inner loop
-    
-    push eax ; push sum
+    push edx ; push sum
     push ebx  ; save i on the stack
     push format  ; push format string
     call printf  ; call printf
     add esp, 12  ; clean up the stack
 
 inner_loop_end:
-    mov edx, 1  ; clear edx for mult
+    mov ecx, 1  ; clear edx for mult
+    xor ebp, ebp
+    xor eax, eax
+    xor edx,edx
+    xor edi,edi
     inc ebx  ; i++
     jmp loop_start  ; go to the next iteration
 
