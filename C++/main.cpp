af139164c9eaf7ebc77c9b1083b135c5f35ee178
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-11 15:08:01 +0300

    /

--- C++/main.cpp
+++ C++/main.cpp
@@ -43,18 +43,21 @@ void Zadanie1_1(void)
 	int a = 0, b = 0, sum = 0, mult = 1;
 	for (int i = 0; i < number.size(); i++)
 	{
-		if (number[i] < 0) break;
-		a = number[i];
-		while (a > 0)
+		if (number[i] > 0)
 		{
-			b = a % 10;// остаток от деления 
-			sum = sum + b;//сумма
-			mult = mult * b;//умножение
-			a = a / 10;// целочисленное деление
-		}
+		a = number[i];
+			while (a > 0)
+			{
+				b = a % 10;// остаток от деления 
+				sum = sum + b;//сумма
+				mult = mult * b;//умножение
+				a = a / 10;// целочисленное деление
+			}
+		
 		if (sum < mult) cout << "Index number = " << i << endl;//вывод индекса элемента
 		sum = 0;
 		mult = 1;
+		}
 	}
 }
 void Zadanie3_21(void)
