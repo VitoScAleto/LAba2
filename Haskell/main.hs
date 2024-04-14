7af52fb31cc705b45c00f338f1fb3547b97783f6
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-14 19:29:50 +0300

    Work

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -2,15 +2,31 @@ import Data.List
 
 main :: IO ()
 main = do
-  let numbers = [76, 24, 614, 14, 1112,56]
-  let result = filterIndices numbers
-  mapM_ print result
+  putStrLn "Введите количество чисел для ввода:"
+  size <- readLn
+  numbers <- getNumbers size []
+  let resultIndexes = findIndexes numbers
+  mapM_ (\i -> putStrLn $ "Index number = " ++ show i) resultIndexes
 
-filterIndices :: [Int] -> [Int]
-filterIndices numbers = findIndices (\x -> sumDigits x < productDigits x) numbers
+getNumbers :: Int -> [Int] -> IO [Int]
+getNumbers 0 acc = return $ reverse acc
+getNumbers n acc = do
+  putStrLn $ "Введите " ++ show n ++ " число:"
+  inputNumber <- readLn
+  getNumbers (n-1) (inputNumber : acc)
+
+findIndexes :: [Int] -> [Int]
+findIndexes numbers = findIndexes' numbers 0
+  where
+    findIndexes' [] _ = []
+    findIndexes' (x:xs) i
+      | x >= 0 = if sumDigits x < multDigits x
+                     then i : findIndexes' xs (i+1)
+                     else findIndexes' xs (i+1)
+      | otherwise = findIndexes' xs (i+1)
 
 sumDigits :: Int -> Int
-sumDigits n = sum $ map (\x -> read [x] :: Int) (show n)
+sumDigits = sum . map (\c -> read [c]) . show
 
-productDigits :: Int -> Int
-productDigits n = product $ map (\x -> read [x] :: Int) (show n)
+multDigits :: Int -> Int
+multDigits = product . map (\c -> read [c]) . show
